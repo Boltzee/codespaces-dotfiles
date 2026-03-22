0006b1405bdf3ba71f9e6be9b3e542fb514ff7c0
Kiran Tirunagiri <tsaikiran506@gmail.com>
2026-03-23 04:35:19 +0530

    lazygit setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,3 +56,9 @@ git submodule update
 # Setup git completions for bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bashrc
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+sudo rm -rf lazygit.tar.gz 
